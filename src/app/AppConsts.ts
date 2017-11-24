9357a1779577e8dff4970b932e974087db178810
Zhangxu <2590000714@qq.com>
2017-11-24 12:38:55 +0800

    删除旧代码，清空项目

--- src/app/AppConsts.ts
+++
@@ -1,5 +0,0 @@
-export class AppConsts {
-    static appBaseUrl: string;
-    static appLanguage: string;
-    static appTitle: string;
-}
--- src/root.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'yaojiangjie-root',
-    template: `<router-outlet></router-outlet>`
-})
-export class RootComponent {
-
-}
