244dba06fe33e4879b06068d7967fc02b0a11fbe
Zhangxu <2590000714@qq.com>
2017-12-06 18:49:23 +0800

    尝试解决扫码之后页面无响应

--- src/shared/services/wechat-scan-qrcode.service.ts
+++ src/shared/services/wechat-scan-qrcode.service.ts
@@ -50,6 +50,7 @@ export class WeChatScanQRCodeService extends AppComponentBase {
 
     scanQRCodeHandler(): void {
         if (this.isWeiXin()) {
+            this._router.navigate(['/index']);
             wx.scanQRCode({
                 needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
                 scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是一维码，默认二者都有
